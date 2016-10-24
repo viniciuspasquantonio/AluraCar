336636a250d032a638d5cf10419006cfa6ed6039
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2016-10-24 21:47:07 -0200

    aula ng-repeat ion-item

---
+++ AluraCar/www/js/controller.js
@@ -0,0 +1,5 @@
+angular.module('starter')
+.controller('ListagemController', function($scope) {
+
+    $scope.listaDeCarros = ['BMW 120i', 'Onix 1.6', 'Fiesta 2.0', 'C3 1.0', 'Uno Fire', 'Sentra 2.0', 'Vectra 2.0 Turbo', 'Hilux 4X4', 'Montana Cabine Dupla', 'Outlander 2.4','Fusca 1500'];
+});
